0652075a5ce878686b9235e9881a9663ad566ce7
jonghwa <jonghwa@pincoin.co.kr>
2024-11-01 02:14:15 +0900

    throw exception

--- src/main/java/kr/co/road2gm/api/domain/auth/controller/AuthController.java
+++ src/main/java/kr/co/road2gm/api/domain/auth/controller/AuthController.java
@@ -7,12 +7,11 @@ import kr.co.road2gm.api.domain.auth.controller.request.PasswordGrantRequest;
 import kr.co.road2gm.api.domain.auth.service.AuthService;
 import kr.co.road2gm.api.global.common.ApiResponse;
 import kr.co.road2gm.api.global.common.constants.ErrorCode;
-import kr.co.road2gm.api.global.error.ErrorResponse;
+import kr.co.road2gm.api.global.error.exception.ApiException;
 import kr.co.road2gm.api.global.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,10 +39,13 @@ public class AuthController {
           PasswordGrantRequest request,
           HttpServletRequest servletRequest,
           HttpServletResponse servletResponse) {
+        // 예외 throw 방식 vs. 오류 객체 조건 분기 방식
+        //
+        // 예외 throw 방식의 장점 - 관심사의 분리, 코드의 분리, 유지보수, AOP 활용
+        // 오류 객체 조건 분기 방식의 장점 - 명시적인 코드 흐름, 스택 트레이스 예외처리 오버헤드 없음
+
         return authService.authenticate(request, servletRequest, servletResponse)
                 .map(response -> ResponseEntity.ok(ApiResponse.success(response)))
-                .orElseGet(() -> ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                        .body(ApiResponse.error(ErrorResponse.of(ErrorCode.WRONG_USERNAME_OR_PASSWORD,
-                                                                 servletRequest.getRequestURI()))));
+                .orElseThrow(() -> new ApiException(ErrorCode.WRONG_USERNAME_OR_PASSWORD));
     }
 }
