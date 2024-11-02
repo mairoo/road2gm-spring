f43e8e566768a7176967b309d317df92474c920e
jonghwa <jonghwa@pincoin.co.kr>
2024-11-02 19:34:51 +0900

    comment fix

--- src/main/java/kr/co/road2gm/api/global/jwt/JwtAuthenticationFilter.java
+++ src/main/java/kr/co/road2gm/api/global/jwt/JwtAuthenticationFilter.java
@@ -48,11 +48,20 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                                                                   null,
                                                                                   userDetails.getAuthorities());
 
-
                     // 6. WebAuthenticationDetails, WebAuthenticationDetailsSource 저장
+                    // WebAuthenticationDetails 객체는 인증 요청과 관련된 웹 관련 정보
+                    // - RemoteAddress (클라이언트 IP 주소)
+                    // - SessionId (현재 세션 ID)
+                    // setDetails()의 활용 용도
+                    // - 특정 IP 주소나 지역에서의 접근 제한
+                    // - 세션 기반의 추가적인 보안 검증
+                    // - 사용자 행동 분석 및 로깅
+                    // - 감사(audit) 기록 생성
+                    // - 다중 요소 인증(MFA) 구현
+                    // - IP 기반 접근 제한이나 차단
                     // authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-                    // 7. 보안 컨텍스트에 사용자 저장 = 로그인 처리
+                    // 7. 현재 인증된 사용자 정보를 보안 컨텍스트에 저장 = 로그인 처리
                     SecurityContextHolder.getContext().setAuthentication(auth);
                 });
 
